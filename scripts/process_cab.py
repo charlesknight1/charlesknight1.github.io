Fix calculation of lengths for cab and kd points

scripts/process_cab.py
@@ -151,10 +151,10 @@ import pandas as pd
 lat = cab_q_da.latitude.values
 lon = cab_q_da.longitude.values
 cab_points = np.where(cab_q_da[0] == 1)
-cab_len = len(cab_points)
+cab_len = len(cab_points[0])
 cab_lat = np.nanmean(lat[cab_points[0]])
 kd_points =  np.where(kd_q_da[0] == 1)
-kd_len = len(kd_points)
+kd_len = len(kd_points[0])
 kd_lat = np.nanmean(lat[kd_points[0]])
 
 history_path = Path("database") / "cab_history.csv"